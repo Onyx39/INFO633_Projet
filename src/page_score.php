417ea5b5465689db5f9ba5c6a03ecedf582a40b7
Onyx39 <valentin.richard39@sfr.fr>
2022-05-10 11:55:43 +0200

    page score bien avance (cube stylé)

--- src/page_score.php
+++ src/page_score.php
@@ -1,10 +1,12 @@
 <style>
     <?php include 'page_score.css'; ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 </style>
-
-<h1>Classements</h1>
-
-<h4>Classement des joueurs</h4>
+<div id='fond'>
+<h1><center>Classements &#127942</center></h1>
 
     <?php 
 
@@ -20,16 +22,87 @@
         /*Encodage UTF8 pour les échanges avec la BD*/
         mysqli_query($conn, "SET NAMES UTF8");
         }
+    ?>
+    
+    <div id='contenu'>
+
+    <?php
 
         $sql_joueur = "SELECT * FROM personne ORDER BY score DESC;";
         $resultat_joueur = mysqli_query($conn, $sql_joueur) or die ("Requête invalide : ".mysqli_error($conn)."\n".$sql_joueur);
-        echo "<div class='classementJoueur'>";
-        echo "<table>";
-        echo "<tr><th>Rang</th><th>Pseudo</th><th>Score</th></tr>";
+        echo "<table border='1'>";
+        echo "<caption align='top'><h4>Classement des joueurs</h4></caption>";
+        echo "<thead>";
+        echo "<tr><th id='rang'>Rang</th><th>Pseudo</th><th>Score</th></tr>";
+        echo "</thead>";
+        echo "<tbody>";
         $rang = 1; 
         while ($row_joueur = mysqli_fetch_assoc($resultat_joueur)) {
-            echo "<tr><td>".$rang."</td><td>".$row_joueur['pseudo']."</td><td>".$row_joueur['score']."</td></tr>";
-            $rang++;
+            if ($rang == 1) {
+                echo "<tr><td>&#129351</td><td>".$row_joueur['pseudo']."</td><td>".$row_joueur['score']."</td></tr>";
+                $rang++;
+            }
+            else {if ($rang == 2) {
+                echo "<tr><td>&#129352</td><td>".$row_joueur['pseudo']."</td><td>".$row_joueur['score']."</td></tr>";
+                $rang++;
+            }
+            else {if ($rang == 3) {
+                echo "<tr><td>&#129353</td><td>".$row_joueur['pseudo']."</td><td>".$row_joueur['score']."</td></tr>";
+                $rang++;
+            }
+            else {if ($rang > 3) {
+                echo "<tr><td>" .$rang."</td><td>".$row_joueur['pseudo']."</td><td>".$row_joueur['score']."</td></tr>";
+                $rang++;
+            }}}}
+
         }
+        echo "</tbody>";
         echo "</table>";
-        echo "</div>";
+
+
+        $sql_equipe = "SELECT * FROM equipe ORDER BY score DESC;";
+        $resultat_equipe = mysqli_query($conn, $sql_equipe) or die ("Requête invalide : ".mysqli_error($conn)."\n".$sql_equipe);
+        echo "<table border='1'>";
+        echo "<caption align='top'><h4>Classement des équipes</h4></caption>";
+        echo "<thead>";
+        echo "<tr><th id='rang'>Rang</th><th>Equipe</th><th>Score</th></tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        $rang2 = 1; 
+        while ($row_equipe = mysqli_fetch_assoc($resultat_equipe)) {
+            if ($rang2 == 1) {
+                echo "<tr><td>&#129351</td><td>".$row_equipe['nom']."</td><td>".$row_equipe['score']."</td></tr>";
+            $rang2++;
+            }
+            else {if ($rang2 == 2) {
+                echo "<tr><td>&#129352</td><td>".$row_equipe['nom']."</td><td>".$row_equipe['score']."</td></tr>";
+            $rang2++;
+            }
+            else {if ($rang2 == 3) {
+                echo "<tr><td>&#129353</td><td>".$row_equipe['nom']."</td><td>".$row_equipe['score']."</td></tr>";
+            $rang2++;
+            }
+            else {if ($rang2 > 3) {
+                echo "<tr><td>" .$rang2."</td><td>".$row_equipe['nom']."</td><td>".$row_equipe['score']."</td></tr>";
+            $rang2++;
+            }}}}
+        }
+        echo "</tbody>";
+        echo "</table>";
+    ?>
+
+    <div class="cube">
+        <div class="side" id="front"></div>
+        <div class="side" id="bottom"></div>
+        <div class="side" id="top"></div>
+        <div class="side" id="left"></div>
+        <div class="side" id="right"></div>
+        <div class="side" id="back"></div>
+    </div>
+        
+    <script src="./assets/jquery.min.js" type="text/javascript" ></script>
+    <script src="./assets/bootstrap.js" type="text/javascript" ></script>
+    <script src="./script.js" type="text/javascript" ></script>
+
+    </div>
+    </div>
