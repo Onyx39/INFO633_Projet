61d7345ab31c7354f1328c96a9123d199aa2586e
Alex <93135705+AlexWallerand@users.noreply.github.com>
2022-05-23 09:48:03 +0200

    Update formulaire.php

--- src/formulaire.php
+++ src/formulaire.php
@@ -7,20 +7,10 @@
         <meta content="info">
         <meta charset="UTF-8">
     </head>
-
-    <?php
-    /*Connection à la BD*/ 
-    $conn = @mysqli_connect("tp-epua:3308", "richaval", "kwia6s9y");
-
-    if (mysqli_connect_errno()) {
-        $msg = "erreur ". mysqli_connect_error();
-    } else {  
-        $msg = "connecté au serveur " . mysqli_get_host_info($conn);
-        mysqli_select_db($conn, "richaval");
     
-        /*Encodage UTF8 pour les échanges avec la BD*/
-        mysqli_query($conn, "SET NAMES UTF8");
-    }
+    <?php
+    require_once("connexion_bdd.php");
+    $conn = connexionBdd();
     ?>
 
     <body>
