51a1e24d9d0e64cb722886da81e3c130f150eac7
Valentin RICHARD <90321867+Onyx39@users.noreply.github.com>
2022-05-23 08:28:43 +0200

    mot de passe table personne

--- src/creationBase.sql
+++ src/creationBase.sql
@@ -2,24 +2,26 @@ DROP TABLE IF EXISTS affectTournoiEquipe;
 DROP TABLE IF EXISTS affectTournoiIndiv;
 DROP TABLE IF EXISTS personne;
 DROP TABLE IF EXISTS equipe;
-DROP TABLE IF EXISTS lieu;
 DROP TABLE IF EXISTS tournoi;
-
-
-CREATE TABLE personne (
-    idPersonne INT(4) AUTO_INCREMENT,
-    pseudo VARCHAR(30) NOT NULL,
-    nom VARCHAR(30) NOT NULL,
-    prenom VARCHAR(30) NOT NULL,
-    score FLOAT,
-    CONSTRAINT pk_personne PRIMARY KEY (idPersonne) 
-);
+DROP TABLE IF EXISTS lieu;
 
 CREATE TABLE equipe (
     idEquipe INT(4) AUTO_INCREMENT,
     nom VARCHAR(30) NOT NULL,
     score FLOAT,
     CONSTRAINT pk_equipe PRIMARY KEY (idEquipe) 
+);
+
+CREATE TABLE personne (
+    idPersonne INT(4) AUTO_INCREMENT,
+    pseudo VARCHAR(30) NOT NULL,
+    mdp VARCHAR(30) NOT NULL,
+    nom VARCHAR(30) NOT NULL,
+    prenom VARCHAR(30) NOT NULL,
+    score FLOAT,
+    equipe INT(4) DEFAULT NULL?
+    CONSTRAINT pk_personne PRIMARY KEY (idPersonne),
+    CONSTRAINT fk_equipe FOREIGN KEY (equipe) REFERENCES equipe(idEquipe) 
 );
 
 CREATE TABLE lieu (
@@ -34,10 +36,13 @@ CREATE TABLE tournoi (
     idTournoi INT(4) AUTO_INCREMENT,
     nom VARCHAR(30) NOT NULL,
     mdp VARCHAR(30) NOT NULL,
+    type VARCHAR(30) NOT NULL,
     format VARCHAR(30) NOT NULL,
     prix FLOAT NOT NULL,
     date DATETIME NOT NULL,
-    CONSTRAINT pk_tournoi PRIMARY KEY (idTournoi) 
+    lieu INT(4) DEFAULT NULL,
+    CONSTRAINT pk_tournoi PRIMARY KEY (idTournoi),
+    CONSTRAINT fk_lieu FOREIGN KEY (lieu) REFERENCES lieu(idLieu) 
 );
 
 CREATE TABLE affectTournoiIndiv (
