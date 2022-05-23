4362807c470ff02d46067750bc8029bc9867246d
Valentin RICHARD <90321867+Onyx39@users.noreply.github.com>
2022-05-23 08:33:51 +0200

    Corrections indices

--- src/remplissageBase.sql
+++ src/remplissageBase.sql
@@ -1,6 +1,6 @@
 INSERT INTO personne (pseudo, mdp, nom, prenom, score) VALUE ('Onyx', 'gfrgf', 'Richard', 'Valentin', 25);
 INSERT INTO personne (pseudo, mdp, nom, prenom, score) VALUE ('Anonymous', 'tructruc$', 'Dupond', 'Jean', 56);
-INSERT INTO personne (pseudo, mdp, nom, prenom, score) VALUE ('Salma', 'Salma' 'IDU', 'Nelma', 89);
+INSERT INTO personne (pseudo, mdp, nom, prenom, score) VALUE ('Salma', 'Salma', 'IDU', 'Nelma', 89);
 
 INSERT INTO equipe (nom, score) VALUES ('Kavé Team', 100);
 INSERT INTO equipe (nom, score) VALUES ('IDU', 80);
@@ -14,11 +14,11 @@ INSERT INTO tournoi (nom, mdp, type, format, prix, date, lieu) VALUES ('Tournoi 
 INSERT INTO tournoi (nom, mdp, type, format, prix, date, lieu) VALUES ('Tournoi de France', 'fg^9$ù$f8', 'equipe', 'chacun pour son equipe', 5, now(), 2);
 INSERT INTO tournoi (nom, mdp, type, format, prix, date, lieu) VALUES ('Tournoi Popo', 'fg^9$ù$f8', 'indiv', 'chacun pour soi', 1, now(), 1);
 
-INSERT INTO affectTournoiIndiv VALUES (0, 2);
-INSERT INTO affectTournoiIndiv VALUES (1, 2);
-INSERT INTO affectTournoiIndiv VALUES (2, 2);
-INSERT INTO affectTournoiIndiv VALUES (1, 1);
+INSERT INTO affectTournoiIndiv VALUES (1, 3);
+INSERT INTO affectTournoiIndiv VALUES (2, 3);
+INSERT INTO affectTournoiIndiv VALUES (3, 3);
+INSERT INTO affectTournoiIndiv VALUES (2, 1);
 
-INSERT INTO affectTournoiEquipe VALUES (0, 1);
-INSERT INTO affectTournoiEquipe VALUES (1, 1);
-INSERT INTO affectTournoiEquipe VALUES (2, 1);
+INSERT INTO affectTournoiEquipe VALUES (1, 2);
+INSERT INTO affectTournoiEquipe VALUES (2, 2);
+INSERT INTO affectTournoiEquipe VALUES (3, 2);
