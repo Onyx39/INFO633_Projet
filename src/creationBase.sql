3296190dd9bd8e9486df01f60ed57b8c544c4ed2
Valentin RICHARD <90321867+Onyx39@users.noreply.github.com>
2022-05-23 08:27:01 +0200

    Ajout mot de passe pour table personne

--- src/creationBase.sql
+++ src/creationBase.sql
@@ -15,10 +15,11 @@ CREATE TABLE equipe (
 CREATE TABLE personne (
     idPersonne INT(4) AUTO_INCREMENT,
     pseudo VARCHAR(30) NOT NULL,
+    mdp VARCHAR(30) NOT NULL,
     nom VARCHAR(30) NOT NULL,
     prenom VARCHAR(30) NOT NULL,
     score FLOAT,
-    equipe INT(4) DEFAULT NULL,
+    equipe INT(4) DEFAULT NULL?
     CONSTRAINT pk_personne PRIMARY KEY (idPersonne),
     CONSTRAINT fk_equipe FOREIGN KEY (equipe) REFERENCES equipe(idEquipe) 
 );
